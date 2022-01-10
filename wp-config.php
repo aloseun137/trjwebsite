25759e3a94104fb8a1969e29fd100164b783ada9
aloseun137 <oluwaseunoffice@gmail.com>
2022-01-10 15:22:40 +0100

    forbidden error fixed

--- wp-config.php
+++ wp-config.php
@@ -33,7 +33,7 @@ define( 'DB_NAME', 'trjcompany' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
