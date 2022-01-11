d2fc57a625d2f05afa88d64fd848bd67f24f9b47
aloseun137 <oluwaseunoffice@gmail.com>
2022-01-11 11:02:45 +0100

    merged conflicts

--- wp-config.php
+++ wp-config.php
@@ -33,7 +33,7 @@ define( 'DB_NAME', 'trjcompany' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
