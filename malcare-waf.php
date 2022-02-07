0734d77bcba77f9aa74ee3583884e1d6157002fa
Alo Seun <oluwaseunoffice@gmail.com>
2022-02-07 18:53:53 +0000

    code injected again

--- malcare-waf.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Please validate auto_prepend_file setting before removing this file
-
-if (file_exists('/var/www/html/wp-content/plugins/blogvault-real-time-backup/protect/prepend/ignitor.php')) {
-	define("MCDATAPATH", '/var/www/html/wp-content/mc_data/');
-	define("MCCONFKEY", '27c6a1019444fdab6a94b1545eb311d6');
-	include_once('/var/www/html/wp-content/plugins/blogvault-real-time-backup/protect/prepend/ignitor.php');
-}
-?>
